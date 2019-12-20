6c5c93b6e8388e6b5fa22f2d0d79617de8d16d5a
Erwin GO <erwingaitano@gmail.com>
2019-12-20 02:11:55 -0500

    added domain route

---
+++ src/_routes/domain.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const validator = require('../_helpers/validator');
+const Domain = require('../_models/Domain');
+
+function handleError(res, err) {
+  try {
+    res.status(500).json(JSON.parse(err.message));
+  } catch(_) {
+    res.status(500).json({ message: err && err.message });
+  }
+}
+
+function createRouter(DBConnection) {
+  const router = express.Router();
+  const DomainModel = Domain.createModel(DBConnection);
+
+  router.use(bodyParser.json());
+
+  router.get('/', (req, res) => {
+    DomainModel.find({}).select('name')
+      .then((domains) => res.json(domains))
+      .catch((err) => handleError(res, err))
+  });
+
+  router.post('/', (req, res) => {
+    validator.requiredObjProps(req.body, ['name'])
+      .then((params) => validator.validateObj(params, {
+        name: { validator: 'isLength', params: [{ min: 5 }]}
+      }))
+      .then(({ name }) => new DomainModel({ name }).save())
+      .then(({ name }) => res.json({ name }))
+      .catch(err => handleError(res, err));
+  });
+
+  router.patch('/:domain', (req, res) => {
+    Promise.resolve(req.params)
+      .then(({ domain }) =>
+        DomainModel.findOneAndUpdate({ domain }, req.body, { new: true }).select('domain')
+      )
+      .then((domain) => res.json(domain))
+      .catch((err) => handleError(res, err))
+  });
+
+  router.delete('/:domain', (req, res) => {
+    DomainModel.deleteOne({ domain: req.params.domain }).select('domain')
+      .then((domain) => res.json(domain))
+      .catch((err) => handleError(res, err))
+  });
+
+  return router;
+}
+
+module.exports = {
+  createRouter
+};
