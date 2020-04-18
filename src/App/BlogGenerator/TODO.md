09f4194fed43bee728eb7c77bc1608f37e12391f
Erwin GO <erwingaitano@gmail.com>
2020-04-17 21:25:06 -0500

    added bin file so I can use this in the cli

--- src/App/BlogGenerator/TODO.md
+++ src/App/BlogGenerator/TODO.md
@@ -1,10 +1,12 @@
 # TODO
-- generate a blog from a folder with md files
+- parse dates
+- sort posts by date
 - remove <base> so you can use references in the same page with anchors<a href="#title">
 - create tags page
 - add highlight lines to code
 
 # DONE
+- generate a blog from a folder with md files
 - highlight syntax for code
 - use development environment using the actual production assets
 - create a module that receives a blog.json and returns a .json file structure 
---
+++ src/App/BlogGenerator/bin.js
@@ -0,0 +1,39 @@
+const { program } = require('commander');
+const path = require('path');
+const helpers = require('./helpers');
+const fsExtra = require('fs-extra');
+
+program
+    .requiredOption(
+        '-i, --inputDirectory <dir>',
+        'Blog directory with the Markdown files'
+    )
+    .option(
+        '-o, --outputDirectory <dir>',
+        'Output directory with generated files'
+    );
+
+program.parse(process.argv);
+
+console.log('Creating blog...');
+
+helpers
+    .generateBlogFileStructureFromDir(path.join(program.inputDirectory))
+    .then((blogFileStructure) => {
+        const outputDirectory = path.resolve(
+            program.outputDirectory
+                ? program.outputDirectory
+                : program.inputDirectory,
+            '_generatedBlog'
+        );
+
+        Object.keys(blogFileStructure).forEach((key) => {
+            fsExtra.outputFileSync(
+                path.join(outputDirectory, key),
+                blogFileStructure[key],
+                'utf8'
+            );
+        });
+
+        console.log('Blog generated at', outputDirectory);
+    });
