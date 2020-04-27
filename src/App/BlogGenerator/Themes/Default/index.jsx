c6b72b71cc79f571305b66c4077bba3bf12eac7b
Erwin GO <erwingaitano@gmail.com>
2020-04-27 18:04:02 -0500

    moved BlogGenerator component out of the project and made it standalone

--- src/App/BlogGenerator/Themes/Default/index.jsx
+++
@@ -1,9 +0,0 @@
-const IndexPage = require('./IndexPage');
-const PostPage = require('./PostPage');
-require('./index.css');
-require('highlight.js/styles/atom-one-dark.css');
-
-module.exports = {
-    PostPage,
-    IndexPage,
-};
